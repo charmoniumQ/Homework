f34aebbe5e8ba1cfa5dc55fa67295b193efaa1b7
Sam G <thegladiusmaximus@gmail.com>
2015-04-04 17:19:16 -0500

    added python code for number theory

--- number_theory/tools.py
+++ number_theory/tools.py
@@ -1,3 +1,10 @@
+from __future__ import print_function
+from math import *
+from itertools import *
+
+def first_n(n, iterator):
+    return (next(iterator) for _ in range(n))
+
 def divides(d, a):
     return a % d == 0
 
@@ -9,12 +16,12 @@ def gcd(a1, b1, printing=True):
     b = min(abs(a1), abs(b1))
     q = a / b
     r = a % b
-    if printing: print '{a} = {b} * {q} + {r}'.format(**locals())
+    if printing: print ('{a} = {b} * {q} + {r}'.format(**locals()))
     if r == 0:
-        if printing: print 'gcd({a}, {b}) = {b} since {b}|{a}'.format(**locals())
+        if printing: print ('gcd({a}, {b}) = {b} since {b}|{a}'.format(**locals()))
         return b
     else:
-        if printing: print 'gcd({a}, {b}) = gcd({b}, {r})'.format(**locals())
+        if printing: print ('gcd({a}, {b}) = gcd({b}, {r})'.format(**locals()))
         return gcd(b, r, printing)
 
 def coprime(a, b):
@@ -26,24 +33,100 @@ def division(m, n):
     assert 0 <= r < n
     return q, r
 
-def linear_diophantine(a, b, c):
-    j = 0
-    x = 0
-    while j % b != c:
-        j += a
-        x += 1
-    y = a * x / b
-    #print j, x, y, a * x, b * y
-    return a, b
-        
+# def linear_diophantine(a, b, c):
+#     j = 0
+#     x = 0
+#     while j % b != c:
+#         j += a
+#         x += 1
+#     y = a * x / b
+#     print a * x, b * y, c
+#     return x, y
+
+def primes():
+    # absolutely no optimization here
+    all_primes = [2]
+    i = all_primes[-1]
+    yield all_primes[-1]
+    while True:
+        i = i + 1
+        for prime in all_primes:
+            if i % prime == 0:
+                break
+        else:
+            all_primes.append(i)
+            yield i
+
+def prime_factorization(n):
+    if n == 1:
+        return []
+    else:
+        for prime in takewhile(lambda prime: prime <= n, primes()):
+            if n % prime == 0:
+                return [prime] + prime_factorization(n / prime)
+
 def is_prime(n):
-    if n < 2:
-        return False
-    if n == 2:
+    return prime_factorization(n) == [n]
+
+def is_composite(n):
+    return len(prime_factorization(n)) > 1
+
+def hilbert_set():
+    h = 1
+    while True:
+        yield h
+        h = h + 4
+
+def hilbert_primes():
+    hilbert_primes = [5]
+    h = hilbert_primes[-1]
+    yield h
+    while True:
+        h = h + 4
+        for hilbert_prime in hilbert_primes:
+            if h % hilbert_prime == 0:
+                break
+        else:
+            hilbert_primes.append(h)
+            yield h
+
+def is_hilbert_prime(h):
+    larger_hilbert_primes = dropwhile(lambda x: x < h, hilbert_primes())
+    if next(larger_hilbert_primes) == h:
         return True
-    if n % 2 == 0:
+    else:
         return False
-    for x in range(3, int(n**.5 + 1), 2):
-        if n % x == 0:
-            return False
-    return True
+
+def hilbert_prime_factorization(n, printing=True):
+    if n == 1:
+        return [[]]
+    else:
+        if is_hilbert_prime(n):
+            return [[n]]
+        else:
+            possibilities = []
+            if printing: print ('factors of {n} less-than-or-equal-to {0}'.format(sqrt(n), **locals()))
+            for hilbert_prime in takewhile(lambda x: x <= sqrt(n), hilbert_primes()):
+                if n % hilbert_prime == 0:
+                    continuation = hilbert_prime_factorization(n / hilbert_prime, printing)
+                    possibilities.append([hilbert_prime, continuation])
+            return possibilities
+
+if __name__ == '__main__':
+
+    # Question: is n! + 1 prime for all n?
+    # we know that there exists a prime between n and n! + 1
+    # but is it n! + 1?
+    for i in count(1):
+        f = factorial(i) + 1
+        if is_composite(f):
+            prime_fac = ' * '.join(map(str, prime_factorization(f)))
+            print ('{i}! + 1 = {f} = {prime_fac}'.format(**locals()))
+            break
+
+    # Question: what are the first ten hilbert primes?
+    print (list(first_n(10, hilbert_primes())))
+
+    # Question: what are the five ten hilbert numbers with multiple prime factorizations
+    # or equivalently: Could Harrelson have used a smaller number than 693?
+    # TODO
