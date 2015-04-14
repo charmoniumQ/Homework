d2ab73bd623b34f8fc7a3159db7f1b68f6cc620d
Sam G <thegladiusmaximus@gmail.com>
2015-04-14 11:19:02 -0500

    wrote ipython script for systems of linear congruences

--- number_theory/tools.py
+++ number_theory/tools.py
@@ -6,16 +6,22 @@ from functools import reduce
 from collections import Counter
 from tools_extra import memoize_generator, memoize_iterator
 
+NaN = None
+
 def first(n):
     return lambda iterator: (next(iterator) for _ in range(n))
 
 def divides(d, a):
+    '''Returns true if d | a'''
     return a % d == 0
 
 def mod(a, b, n):
+    '''Returns true if a = b mod n'''
     return divides(n, a - b)
 
-def gcd(a1, b1, printing=True):
+def gcd(a1, b1, printing=False):
+    '''Returns the greatest common multiple'''
+    # WLOG a > b > 0
     a = max(abs(a1), abs(b1))
     b = min(abs(a1), abs(b1))
     q = a / b
@@ -27,27 +33,31 @@ def gcd(a1, b1, printing=True):
         if printing: print('gcd({a}, {b}) = gcd({b}, {r}) since {a} = {b} * {q} + {r}'.format(**locals()))
         return gcd(b, r, printing)
 
+def lcm(a, b):
+    '''Returns the least common positive multiple'''
+    return abs(a * b) / gcd(a, b)
+
 def coprime(a, b):
+    '''Returns if a and b are coprime'''
     return gcd(a, b) == 1
 
 def division(m, n):
+    '''Returns the q and r from the dividion algorithm for'''
     q = m / n
     r = m - n*q
-    assert 0 <= r < n
+    #assert 0 <= r < n
     return q, r
 
-def linear_diophantine(a, b, c):
-    for x in count():
-        if a * x % b == c:
-            break
-    return x, (a*x - c) / b
+def cmod(a, n):
+    '''Returns a = c mod n where 0 <= c < n'''
+    return division(a, n)[1]
 
 @memoize_generator
 def primes():
-    # absolutely no optimization here
+    '''Return a generator of primes'''
+    yield 2
     all_primes = [2]
     i = all_primes[-1]
-    yield all_primes[-1]
     while True:
         i = i + 1
         for prime in all_primes:
@@ -58,18 +68,31 @@ def primes():
             yield i
 
 def prime_factorization(n):
+    '''Returns the prime factorization of any integer n'''
+    # for natural numbers
+    if n < 0:
+        return [-1] + prime_factorization(-n)
     if n == 1:
         return []
     else:
-        for prime in takewhile(lambda prime: prime <= n, primes()):
+        for prime in takewhile(lambda prime: prime <= abs(n), primes()):
             if n % prime == 0:
                 return [prime] + prime_factorization(n / prime)
-
+            
 def is_prime(n):
-    return prime_factorization(n) == [n]
+    '''Returns true if n is prime for any natural n'''
+    larger_primes = dropwhile(lambda x: x < n, primes())
+    if next(larger_primes) == n:
+        return True
+    else:
+        return False
 
 def is_composite(n):
-    return len(prime_factorization(n)) > 1
+    '''Returns true if n is composite for any natural n'''
+    return n != 1 and not is_prime(n)
+
+def primorial(n):
+    return reduce(mul, first(n + 1)(primes()), 1)
 
 def hilbert_set():
     h = 1
@@ -98,41 +121,60 @@ def is_hilbert_prime(h):
     else:
         return False
 
-def hilbert_prime_factorization(n, printing=False):
-    if n == 1:
-        return [[]]
-    else:
-        if is_hilbert_prime(n):
-            return [[n]]
-        else:
-            possibilities = []
-            if printing: print('factors of {n} less-than-or-equal-to {0}'.format(sqrt(n), **locals()))
-            for hilbert_prime in takewhile(lambda x: x <= sqrt(n), hilbert_primes()):
-                if n % hilbert_prime == 0:
-                    continuation = hilbert_prime_factorization(n / hilbert_prime, printing)
-                    possibilities.append([hilbert_prime, continuation])
-            return possibilities
-
 def hilbert_prime_factorization(n, prefix=None):
+    '''Returns a set tuples, where each tuple is a possible hilbert factorizations for any natural number n'''
     if prefix is None:
         prefix = []
     if is_hilbert_prime(n):
-        factorizations = [sorted(prefix + [n])]
-        return factorizations
+        factorization = [tuple(sorted(prefix + [n])), ]
+        return factorization
     else:
-        possibilities = []
+        possibilities = set()
         hilbert_primes_truncated = takewhile(lambda x: x <= sqrt(n), hilbert_primes())
         for hilbert_prime in hilbert_primes_truncated:
             if n % hilbert_prime == 0:
                 quotient = int(n / hilbert_prime)
                 factorizations = hilbert_prime_factorization(quotient, prefix + [hilbert_prime])
-                for factorization in factorizations:
-                    if factorization not in possibilities:
-                        possibilities.append(factorization)
+                possibilities = possibilities.union(factorizations)
         return possibilities
 
-def primorial(n):
-    return reduce(mul, first(n)(primes()), 1)
+def linear_diophantine(a, b, c):
+    '''Return (x0, y0), (xi, yi) where (x0 + n * xi) * a + (y0 + n * yi) * b = c for integer n'''
+    g = gcd(a, b)
+    if not divides(g, c):
+        return None
+    for x in count():
+        if a * x % b == g:
+            break
+    y = (g - a*x) / b
+    return (x * c / g, y * c / g), (b / g, -a / g)
+
+def linear_congruence(a, b, n):
+    '''Return x0, xi where a * (x0 + n * xi) = b mod n for integer n'''
+    # ax = b mod n
+    # b = ax mod n
+    # n|(b - ax)
+    # yn = b - ax
+    # ax + yn = b
+    (x0, y0), (xi, yi) = linear_diophantine(a, n, b)
+    return x0, xi
+
+def linear_congruence_system(eqns, printing=False):
+    # if x = x0 + j * xi, then x = all whole numbers
+    # therefore start with x0 = 0, xi = 1
+    x0, xi = 0, 1
+    for a, b, n in eqns:
+        for j in count():
+            if mod(a * (x0 + j*xi), b, n):
+                if printing: print('x = {' + str(list(map(lambda j: x0 + j * xi, xrange(0, 3 * j + 3))))[1:-1] + ', ...}')
+                x0 = x0 + j * xi
+                xi = lcm(xi, n)
+                if printing: print('x satisfies {a} * x = {b} mod {n} and all previous equations'.format(**locals()))
+                if printing: print('when x = {x0} + j * {xi}'.format(**locals()))
+                break
+        else:
+            print('death')
+    return x0, xi
 
 def main():
     # Question: is n! + 1 prime for all n?
@@ -175,6 +217,26 @@ def main():
             print(h, hpf)
         if len(numbers) > 10:
             break
+    print('')
+
+    # Task: write a program that solves linear diophantine equations
+    # this makes the exercises much easier
+    print('Linear diophantine solutions:')
+    a, b, c = 7, 8, 100
+    (x0, y0), (xi, yi) = linear_diophantine(a, b, c)
+    x1, y1 = x0 + xi, y0 + yi
+    print('{a}*{x0} + {b}*{y0} = {c}'.format(**locals()))
+    print(a*x0 + b*y0 == c)
+    print('{a}*{x1} + {b}*{y1} = {c}'.format(**locals()))
+    print(a*x1 + b*y1 == c)
+    print('')
+
+    print('Systems of linear congruence solutions:')
+    linear_congruence_system([(1, 3, 17), (1, 10, 16), (1, 0, 15)])
+    linear_congruence_system([(1, 1, 2), (1, 2, 3), (1, 3, 4), (1, 4, 5), (1, 5, 6)])
+    # I am not going to test this. I know it works.
+    # (famous last words)
+    print('')
 
 if __name__ == '__main__':
     pass
